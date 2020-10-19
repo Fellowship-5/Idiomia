c981f7817836e9607891a5f072cddadd479d4be2
Ammar <zarosho@gmail.com>
2020-10-19 18:51:12 +0200

    end points log in and sign up are done

---
+++ db/mongodb.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useCreateIndex', true);
+
+mongoose
+	.connect(
+		`mongodb+srv://HYF-Ammar:${process.env.MongoDB_password}@cluster0-ptw2q.azure.mongodb.net/${process.env
+			.MongoDb_collection}?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false
+		}
+	)
+	.then(() => {
+		console.log('connected to db');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+module.exports = mongoose;
