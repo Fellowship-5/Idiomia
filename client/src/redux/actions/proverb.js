b1498b0668fd15f41790538b6014508cb6c94bbc
salih <salihsert18@gmail.com>
2020-10-26 21:52:58 +0100

    Create proverb states,actions,reducers and connect backend

---
+++ client/src/redux/actions/proverb.js
@@ -0,0 +1,170 @@
+import axios from "axios";
+import {
+  GET_PROVERBS,
+  GET_PROVERBS_SUCCESS,
+  GET_PROVERBS_ERROR,
+  GET_USER_PROVERBS,
+  GET_USER_PROVERBS_SUCCESS,
+  GET_USER_PROVERBS_ERROR,
+  GET_PROVERB,
+  GET_PROVERB_SUCCESS,
+  GET_PROVERB_ERROR,
+  ADD_PROVERB,
+  ADD_PROVERB_SUCCESS,
+  ADD_PROVERB_ERROR,
+  DELETE_PROVERB,
+  DELETE_PROVERB_SUCCESS,
+  DELETE_PROVERB_ERROR,
+  UPDATE_PROVERB,
+  UPDATE_PROVERB_SUCCESS,
+  UPDATE_PROVERB_ERROR,
+} from "./types";
+import { toast } from "react-toastify";
+
+const API_URL = process.env.REACT_APP_IDIOMIA_API;
+
+// Get All Proverbs
+export const getProverbs = (...args) => async (dispatch) => {
+  if (args[0] === "userProverb") {
+    try {
+      dispatch({
+        type: GET_USER_PROVERBS,
+      });
+      const res = await axios.get(`${API_URL}/proverbs/my-proverbs`);
+      dispatch({
+        type: GET_USER_PROVERBS_SUCCESS,
+        payload: res.data.user_proverbs,
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_USER_PROVERBS_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  } else {
+    try {
+      dispatch({
+        type: GET_PROVERBS,
+      });
+      const res = await axios.get(`${API_URL}/proverbs/all-proverbs`);
+      dispatch({
+        type: GET_PROVERBS_SUCCESS,
+        payload: res.data.proverbs,
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_PROVERBS_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  }
+};
+
+// Get Proverb
+export const getProverb = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_PROVERB,
+    });
+    const res = await axios.get(`${API_URL}/xxx`); //@not ready
+
+    dispatch({
+      type: GET_PROVERB_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_PROVERB_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add proverb
+export const addProverb = (formData) => async (dispatch, getState) => {
+  const { isAuthenticated } = getState().auth;
+  try {
+    dispatch({
+      type: ADD_PROVERB,
+    });
+    const postUrl = isAuthenticated
+      ? `${API_URL}/proverbs/post-my-proverb`
+      : `${API_URL}/proverbs/post-proverb`;
+    const res = await axios.post(postUrl, formData);
+
+    dispatch({
+      type: ADD_PROVERB_SUCCESS,
+      payload: res.data.proverb,
+    });
+
+    toast.success("Proverb added successfully");
+  } catch (err) {
+    const { errors, msg } = err.response.data;
+
+    if (errors) {
+      errors.forEach((error) => toast.error(error.msg));
+    }
+    if (msg) {
+      toast.error(msg);
+    }
+
+    dispatch({
+      type: ADD_PROVERB_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete proverb
+export const deleteProverb = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: DELETE_PROVERB,
+    });
+    await axios.delete(`${API_URL}/proverbs/delete-my-proverb/${id}`);
+
+    dispatch({
+      type: DELETE_PROVERB_SUCCESS,
+      payload: id,
+    });
+
+    toast.success("Proverb deleted successfully");
+  } catch (err) {
+    dispatch({
+      type: DELETE_PROVERB_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Update Proverb
+export const updateProverb = (formData, id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: UPDATE_PROVERB,
+    });
+    const res = await axios.put(
+      `${API_URL}/proverbs/edit-my-proverb/${id}`,
+      formData,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_PROVERB_SUCCESS,
+      payload: res.data,
+    });
+
+    toast.success("Proverb updated successfully");
+  } catch (err) {
+    if (errors) {
+      errors.forEach((error) => toast.error(error.msg));
+    }
+    if (msg) {
+      toast.error(msg);
+    }
+    dispatch({
+      type: UPDATE_PROVERB_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
--- client/src/redux/hooks.js
+++ client/src/redux/hooks.js
@@ -1,54 +1,123 @@
-import { useCallback } from 'react';
-import { useDispatch, shallowEqual, useSelector } from 'react-redux';
-import { loadUser, register, login, logout } from './actions/auth';
+import { useCallback } from "react";
+import { useDispatch, shallowEqual, useSelector } from "react-redux";
+import { loadUser, register, login, logout } from "./actions/auth";
+import {
+  getProverbs,
+  addProverb,
+  deleteProverb,
+  updateProverb,
+  getProverb,
+} from "./actions/proverb";
 
 export function useAuth() {
-    const dispatch = useDispatch();
-    const { token, isAuthenticated, loading, user } = useSelector(
-      (state) => ({
-        token: state.auth.token,
-        isAuthenticated: state.auth.isAuthenticated,
-        loading: state.auth.loading,
-        user: state.auth.user
-      }),
-      shallowEqual
-    );
-  
-    const boundLoadUser = useCallback(
-      (...args) => {
-        return dispatch(loadUser(...args));
-      },
-      [dispatch]
-    );
-  
-    const boundRegisterUser = useCallback(
-      (...args) => {
-        return dispatch(register(...args));
-      },
-      [dispatch]
-    );
-  
-    const boundLoginUser = useCallback(
-      (...args) => {
-        return dispatch(login(...args));
-      },
-      [dispatch]
-    );
-    const boundLogoutUser = useCallback(
-      (...args) => {
-        return dispatch(logout(...args));
-      },
-      [dispatch]
-    );
-  
-    return {
-      token,
-      isAuthenticated,
-      loading,
-      user,
-      loadUser: boundLoadUser,
-      registerUser: boundRegisterUser,
-      loginUser: boundLoginUser,
-      logoutUser: boundLogoutUser
-    };
-  }
+  const dispatch = useDispatch();
+  const { token, isAuthenticated, loading, user } = useSelector(
+    (state) => ({
+      token: state.auth.token,
+      isAuthenticated: state.auth.isAuthenticated,
+      loading: state.auth.loading,
+      user: state.auth.user,
+    }),
+    shallowEqual
+  );
+
+  const boundLoadUser = useCallback(
+    (...args) => {
+      return dispatch(loadUser(...args));
+    },
+    [dispatch]
+  );
+
+  const boundRegisterUser = useCallback(
+    (...args) => {
+      return dispatch(register(...args));
+    },
+    [dispatch]
+  );
+
+  const boundLoginUser = useCallback(
+    (...args) => {
+      return dispatch(login(...args));
+    },
+    [dispatch]
+  );
+  const boundLogoutUser = useCallback(
+    (...args) => {
+      return dispatch(logout(...args));
+    },
+    [dispatch]
+  );
+
+  return {
+    token,
+    isAuthenticated,
+    loading,
+    user,
+    loadUser: boundLoadUser,
+    registerUser: boundRegisterUser,
+    loginUser: boundLoginUser,
+    logoutUser: boundLogoutUser,
+  };
+}
+
+export function useProverb() {
+  const dispatch = useDispatch();
+  const { proverbs, userProverbs, proverb, loading, error } = useSelector(
+    (state) => ({
+      proverbs: state.proverb.proverbs,
+      userProverbs: state.proverb.userProverbs,
+      proverb: state.proverb.proverb,
+      loading: state.proverb.loading,
+      error: state.proverb.error,
+    }),
+    shallowEqual
+  );
+
+  const boundGetProverbs = useCallback(
+    (...args) => {
+      return dispatch(getProverbs(...args));
+    },
+    [dispatch]
+  );
+
+  const boundGetProverb = useCallback(
+    (...args) => {
+      return dispatch(getProverb(...args));
+    },
+    [dispatch]
+  );
+
+  const boundAddProverb = useCallback(
+    (...args) => {
+      return dispatch(addProverb(...args));
+    },
+    [dispatch]
+  );
+
+  const boundDeleteProverb = useCallback(
+    (...args) => {
+      return dispatch(deleteProverb(...args));
+    },
+    [dispatch]
+  );
+
+  const boundUpdateProverb = useCallback(
+    (...args) => {
+      return dispatch(updateProverb(...args));
+    },
+    [dispatch]
+  );
+
+  return {
+    proverbs,
+    userProverbs,
+    proverb,
+    loading,
+    error,
+    getProverbs: boundGetProverbs,
+    getProverb: boundGetProverb,
+    addProverb: boundAddProverb,
+    deleteProverb: boundDeleteProverb,
+    updateProverb: boundUpdateProverb,
+  };
+}
