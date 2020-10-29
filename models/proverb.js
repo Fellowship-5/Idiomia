4ab175fa1e4ef5f2d2497d4b1e6e630ff406fe7c
salih <salihsert18@gmail.com>
2020-10-29 10:55:47 +0100

    FIx merge conflicts

--- models/proverb.js
+++ models/proverb.js
@@ -1,22 +1,17 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const proverbSchema = new Schema({
-	proverb: { type: String, required: true, maxlength: 250 },
-	translation: { type: String, required: true, maxlength: 250 },
-	explanation: { type: String, required: true },
-<<<<<<< HEAD
-	adminApproval: { type: Boolean, required: false },
-	contributor: { type: mongoose.Types.ObjectId, required: false, ref: 'User' },
-	date: { type: Date }
-=======
-	adminApproval: { type: Boolean, required: false, default: false },
-	contributor: { type: mongoose.Types.ObjectId, required: false, ref: 'User' }
->>>>>>> admin_endpoints_approving_proverbs
+  proverb: { type: String, required: true, maxlength: 250 },
+  translation: { type: String, required: true, maxlength: 250 },
+  explanation: { type: String, required: true },
+  adminApproval: { type: Boolean, required: false, default: false },
+  contributor: { type: mongoose.Types.ObjectId, required: false, ref: "User" },
+  date: { type: Date },
 });
 
 proverbSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('Proverb', proverbSchema);
+module.exports = mongoose.model("Proverb", proverbSchema);
