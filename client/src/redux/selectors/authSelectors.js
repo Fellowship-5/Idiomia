95f0aa6c48f621a5f1d49f712c83dc55efa5fbbb
salih <salihsert18@gmail.com>
2020-10-29 23:00:01 +0100

    implement changes due to the feedback

--- client/src/redux/selectors/authSelectors.js
+++ client/src/redux/selectors/authSelectors.js
@@ -1,7 +1,2 @@
-export const selectAuth =  (state) => ({
-    token: state.auth.token,
-    isAuthenticated: state.auth.isAuthenticated,
-    loading: state.auth.loading,
-    user: state.auth.user,
-  })
+export const selectAuth =  (state) => (state.auth)
 
--- client/src/redux/selectors/proverbSelectors.js
+++ client/src/redux/selectors/proverbSelectors.js
@@ -1,7 +1,1 @@
-export const selectProverb =  (state) => ({
-    proverbs: state.proverb.proverbs,
-    userProverbs: state.proverb.userProverbs,
-    proverb: state.proverb.proverb,
-    loading: state.proverb.loading,
-    error: state.proverb.error,
-  })
+export const selectProverb =  (state) => (state.proverb)
