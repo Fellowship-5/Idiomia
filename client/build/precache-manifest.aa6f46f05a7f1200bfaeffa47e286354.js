b49971907ab3c7551ad2ea0668c0b8c2ce89fdd2
Ammar <zarosho@gmail.com>
2020-11-20 11:05:41 +0100

    fixed dependencies

---
+++ client/build/precache-manifest.aa6f46f05a7f1200bfaeffa47e286354.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dc6d6e20646ef77adf4db0385b1857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf7db2d92a2d62f1ead",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6bdd40361c1fbc9810d9",
+    "url": "/static/css/main.270a834c.chunk.css"
+  },
+  {
+    "revision": "2bf7db2d92a2d62f1ead",
+    "url": "/static/js/2.49f248ab.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/static/js/2.49f248ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bdd40361c1fbc9810d9",
+    "url": "/static/js/main.c70890fe.chunk.js"
+  },
+  {
+    "revision": "835514f2ecfde67f2112",
+    "url": "/static/js/runtime-main.5e26b8dd.js"
+  },
+  {
+    "revision": "c25726178ea0c51e265d081612a9da78",
+    "url": "/static/media/backgroun-texture-section.c2572617.png"
+  },
+  {
+    "revision": "51931731f951420e2e9134b67a094892",
+    "url": "/static/media/background-texture-table-row.51931731.png"
+  }
+]);
